6ca475d2f857aa76f83f026704c7d377f7dd6bc2
Hitendra Patel <hitendra.softwareengg@gmail.com>
2026-04-23 17:37:08 +0530

    Delete state.py

--- state.py
+++
@@ -1,8 +0,0 @@
-from langgraph.graph import MessagesState
-from typing import Optional
-
-class SupportState(MessagesState):
-    user_name: Optional[str] = None
-    intent: Optional[str] = None
-    turn_count: int = 0
-    summary: Optional[str] = None
